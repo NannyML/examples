d8c03f7c3e36653850f22b738becebd9e0a01e2b
niels <niels@nannyml.com>
2022-11-21 00:27:53 +0100

    Fix database permission issues

--- multiclass_classification/db/ddl.sql
+++ multiclass_classification/db/ddl.sql
@@ -4,6 +4,5 @@
  * License: Apache Software License 2.0
  */
 
-CREATE DATABASE nannyml;
 CREATE USER nannyml WITH ENCRYPTED PASSWORD 'we<3nannyml';
-GRANT ALL PRIVILEGES ON DATABASE nannyml TO nannyml;
+CREATE DATABASE nannyml OWNER nannyml;
